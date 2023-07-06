85a838b39e08a4bf424813e78aa45fce2668283e
Michael <michael.c.hadley@gmail.com>
2023-07-06 14:13:48 -0400

    Add error messages to utils

--- utils/Utils.js
+++ utils/Utils.js
@@ -10,6 +10,33 @@ function responseHandler(statusCode, message) {
     return response;
 }
 
+function genericErrorMessage(location, param) {
+    const message = {
+        statusCode: 400,
+        body: JSON.stringify({
+            location: location,
+            param: param,
+            message: `There is a problem with ${param} at ${location}`,
+        }),
+    };
+    return message;
+}
+
+function requiredError(location, param) {
+    const message = {
+        statusCode: 400,
+        body: JSON.stringify({
+            location: location,
+            param: param,
+            message: `${param} is required`,
+        }),
+    };
+
+    return message;
+}
+
 module.exports = {
     responseHandler,
+    genericErrorMessage,
+    requiredError,
 };
